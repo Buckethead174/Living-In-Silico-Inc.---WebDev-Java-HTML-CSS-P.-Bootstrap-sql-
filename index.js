b6419b44548bf975e1b55cc9fbb30722d1096cc8
cjarvis6 <cjarvis6@asu.edu>
2024-11-04 23:33:43 -0700

    Added handlebar support for communication from backed to frontend
    Fixed fixed backend, server starts with npm start
    Need to figure out how to install smina
    User input needs to be handled

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import express from 'express'
-import sqlite3 from 'sqlite3'
-import bodyParser from 'body-parser'
-import {open} from 'sqlite'
-
-const dbPromise = await open({
-    filename: './data.db',
-    driver: sqlite3.Database
-})//end of sqlite driver
+const express = require('express')
+const path = require('path')
+const engine = require('express-handlebars')
 
 const app = express()
 const port = 8080
 
-app.use(express.static("static"))
+app.engine("handlebars", engine.engine())
+app.set("view engine", "handlebars")
+app.set("views", path.join(__dirname, "views"))
+
+app.use('/public', express.static('./public'));
 
 app.get('/', (req,res) =>{
-
+    const context = {
+        name: "Jon Doe"
+    }
+    res.render("home", context)
 })
 
-async function setup() {
-    const db = await dbPromise;
-    //set up migrations
+//Useful for setting up the initlization for sever
+//listening and port messaging
+//Potentially migration support goes in here.
+async function setup() 
+{
     app.listen(port, () => {console.log("listening on " + port)})
 }
+
+setup();
