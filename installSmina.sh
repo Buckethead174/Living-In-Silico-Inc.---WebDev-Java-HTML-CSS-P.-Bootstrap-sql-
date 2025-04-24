59fb3e24d54e76fbe32da41ff48a8ceecca2ee82
cjarvis6 <cjarvis6@asu.edu>
2025-04-24 13:02:09 -0700

    fixed error

--- installSmina.sh
+++ installSmina.sh
@@ -1,3 +1,3 @@
 mkdir -p ./sminaBin
-wget https://sourceforge.net/projects/smina/files/smina.static/download -0 ./sminaBin/smina
+wget https://sourceforge.net/projects/smina/files/smina.static/download -o ./sminaBin/smina
 chmod +x ./smina-bin/smina
